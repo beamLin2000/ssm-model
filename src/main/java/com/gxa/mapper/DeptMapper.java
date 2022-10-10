08a2a61ef8c302a9a3a7cb4267a40215d3c8e3f8
beam <1126719760@qq.com>
2022-10-10 17:01:48 +0800

    修改了一些配置

--- src/main/java/com/gxa/mapper/DeptMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gxa.mapper;
-
-import com.gxa.entity.Dept;
-
-import java.util.List;
-
-public interface DeptMapper {
-    List<Dept> queryAll();
-}
--- src/main/java/com/gxa/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.gxa.service;
-
-import com.gxa.entity.User;
-
-public interface UserService {
-    User login(String userName, String pwd);
-}
