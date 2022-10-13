a7dc03362e9c94a42af99ec7a0e5a6aaedeb434e
wulux <ganbeixd@163.com>
2022-10-13 20:12:20 +0800

    swagger

--- src/main/java/com/gxa/entity/drugRetail/DrugQueryCondition.java
+++ src/main/java/com/gxa/entity/drugRetail/DrugQueryCondition.java
@@ -1,5 +1,7 @@
 package com.gxa.entity.drugRetail;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel("药品查询条件实体")
 public class DrugQueryCondition {
+    @ApiModelProperty(name = "chargeCategory",value = "类别")
     private String chargeCategory;
+    @ApiModelProperty(name = "drugCode",value = "药品编码")
     private String drugCode;
+    @ApiModelProperty(name = "drugName",value = "药品名称")
     private String drugName;
 }
