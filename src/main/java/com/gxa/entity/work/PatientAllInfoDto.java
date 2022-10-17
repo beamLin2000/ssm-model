2d0756334d8d1fbcb10df60b30bdba0f8129e0ef
beam <1126719760@qq.com>
2022-10-17 18:56:19 +0800

    完成保存

---
+++ src/main/java/com/gxa/entity/work/PatientAllInfoDto.java
@@ -0,0 +1,21 @@
+package com.gxa.entity.work;
+
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class PatientAllInfoDto {
+    private WorkPatient patient;
+    private MedicalRecordPhysical medicalRecordPhysical;
+    private List<Charge> charges;
+    private List<MedicalCharge> medicalCharges;
+    private ItemCharge itemCharge;
+    private Prescriptions prescriptions;
+}
